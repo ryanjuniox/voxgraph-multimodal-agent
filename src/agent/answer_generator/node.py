7a2cbbee0c42b067cfe2cdbb3e6b0ee16fad889d
ryanjuniox <ryanjunio848@gmail.com>
2026-06-05 20:11:58 -0300

    feat: Implement answer generator with Ollama model integration

--- src/agent/answer_generator/node.py
+++ src/agent/answer_generator/node.py
@@ -1,4 +1,15 @@
+from pydantic_ai.models.ollama import OllamaModel
+from pydantic_ai.providers.ollama import OllamaProvider
+
+from src.utils.constants import OLLAMA_BASE_URL, MODEL_ANSWER_GENERATOR
+
+
 class AnswerGenerator:
-    def answer_generator(state: dict) -> dict:
-        # TODO: chamar a LLM
-        return {"answer": "resposta"}
+    def __init__(self):
+        self.model = OllamaModel(
+            MODEL_ANSWER_GENERATOR, provider=OllamaProvider(base_url=OLLAMA_BASE_URL)
+        )
+
+    def answer_generator(self, state: dict) -> dict:
+        # TODO Retorno agent
+        pass
---
+++ src/utils/constants.py
@@ -0,0 +1,5 @@
+# models
+MODEL_ANSWER_GENERATOR = "gemma4:e2b"
+
+# urls
+OLLAMA_BASE_URL = "http://localhost:11434"
